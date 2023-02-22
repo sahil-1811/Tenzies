3605d592465d408214406fad256dfd8542ed9ef1
Sahil Mody <sahilmody1234@gmail.com>
2023-02-21 20:04:52 -0500

    Updated the Hold Part

--- src/App.js
+++ src/App.js
@@ -6,27 +6,47 @@ import {nanoid} from "nanoid"
 function App() {
 
   const[dice,setdice]=React.useState(allNewDice())
+  
+  function generatenewDice(){
+    return {
+      value:Math.ceil(Math.random()*6),
+      isHeld:false,
+      id:nanoid()
+    }
+  }
+  
   function allNewDice(){
     const result=[]
     for (let i=0;i<10;i++){
       result.push({
         value:Math.ceil(Math.random()*6),
-        isHeld:true,
+        isHeld:false,
         id:nanoid()
       })
     }
     return result
   }
 
-  const diceElements=dice.map(die=><Die key = {die.id} value={die.value} held={die.isHeld} />)
+  const diceElements=dice.map(die=><Die key = {die.id} value={die.value} held={die.isHeld} holddice={()=>holdDice(die.id)} />)
 
+ 
   function reroll(){
-    setdice(allNewDice)
+    setdice(newDice=>newDice.map(die=>{
+      return die.isHeld?die:generatenewDice()
+    }))
   }
 
 
+  function holdDice(id){
+    setdice(oldDice=>oldDice.map(die=>{
+      return die.id===id?{...die,isHeld:!die.isHeld}:die
+    }))
+  }
+
   return (
     <main>
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className="dice-container">
         {diceElements}
       </div>
--- src/Die.js
+++ src/Die.js
@@ -5,7 +5,11 @@ export default function Die(props){
         backgroundColor: props.held?"#59E391":"white"
     }
     return (
-            <div className='die-face' style={styles}>
+            <div 
+                className='die-face' 
+                style={styles}
+                onClick={props.holddice}
+            >
                 <h2 className='die-num'>{props.value}</h2>
             </div>
           
